72be0aeb4e0e2e3250936b03b14344183c532fa4
Grammy Jiang <grammy.jiang@gmail.com>
2018-07-17 19:01:43 +0800

    Make InMemoryStorage class inherent BaseStorage
    Since in the previous commit a new base class for storage object is
    added, the class of InMemoryStorage inherent this new BaseStorage to
    enable it with the methods of BaseStorage.

--- scrapy_cookies/storage/in_memory.py
+++ scrapy_cookies/storage/in_memory.py
@@ -2,14 +2,10 @@ from collections import defaultdict
 
 from scrapy.http.cookies import CookieJar
 
+from scrapy_cookies.storage import BaseStorage
 
-class InMemoryStorage(defaultdict):
+
+class InMemoryStorage(defaultdict, BaseStorage):
     def __init__(self, settings):
         super(InMemoryStorage, self).__init__(CookieJar)
         self.settings = settings
-
-    def open_spider(self, spider):
-        pass
-
-    def close_spider(self, spider):
-        pass
