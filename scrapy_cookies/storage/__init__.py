55a3ded7f3eb9416fb0704c8eb019efc08166cfc
Grammy Jiang <grammy.jiang@gmail.com>
2018-07-17 18:53:05 +0800

    Add a base class for the storage object
    Add a base class for the storage object, which inherents
    MutableMapping.
    
    And also in this base class, a logger is included, which is actually
    copied from Spider class.

---
+++ scrapy_cookies/storage/__init__.py
@@ -0,0 +1,44 @@
+import logging
+from collections import MutableMapping
+
+
+class BaseStorage(MutableMapping):
+    name = None
+
+    def __init__(self, settings):
+        self.settings = settings
+
+    @property
+    def logger(self):
+        logger = logging.getLogger(self.name)
+        return logging.LoggerAdapter(logger, {'storage': self})
+
+    def log(self, message, level=logging.DEBUG, **kw):
+        """Log the given message at the given log level
+
+        This helper wraps a log call to the logger within the storage, but you
+        can use it directly (e.g. Storage.logger.info('msg')) or use any other
+        Python logger too.
+        """
+        self.logger.log(level, message, **kw)
+
+    def open_spider(self, spider):
+        pass
+
+    def close_spider(self, spider):
+        pass
+
+    def __delitem__(self, v):
+        pass
+
+    def __getitem__(self, k):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __setitem__(self, k, v):
+        pass
