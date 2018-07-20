1f6a304c8217de6c4d8565103e16b1cb24ac2f22
Grammy Jiang <grammy.jiang@gmail.com>
2018-07-20 20:44:00 +0800

    Reformat settings.py

--- scrapy_cookies/settings/default_settings.py
+++ scrapy_cookies/settings/default_settings.py
@@ -1,7 +1,11 @@
 COOKIES_ENABLED = True
 COOKIES_DEBUG = False
 
-COOKIES_STORAGE = 'scrapy_cookies.storage.in_memory.InMemoryStorage'
-
 COOKIES_PERSISTENCE = False
 COOKIES_PERSISTENCE_DIR = 'cookies'
+
+# ------------------------------------------------------------------------------
+# IN MEMORY STORAGE
+# ------------------------------------------------------------------------------
+
+COOKIES_STORAGE = 'scrapy_cookies.storage.in_memory.InMemoryStorage'
